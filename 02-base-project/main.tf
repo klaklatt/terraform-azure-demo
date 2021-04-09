5de372dfe519742ff6d514a39f3ab8c6b52a3376
Klaus Klatt <kklatt@ovb.de>
2021-04-09 17:45:06 +0200

    add bastion host

--- 02-base-project/main.tf
+++ 02-base-project/main.tf
@@ -56,9 +56,73 @@ resource "azurerm_network_security_group" "public" {
   name                = "public-subnet-nsg"
   location            = azurerm_resource_group.project.location
   resource_group_name = azurerm_resource_group.project.name
+
+  security_rule {
+    name                       = "AllowSSHInBound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "22"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_subnet_network_security_group_association" "public" {
   subnet_id                 = azurerm_subnet.public.id
   network_security_group_id = azurerm_network_security_group.public.id
+}
+
+resource "azurerm_public_ip" "bastion" {
+  name                = "Bastion-PIP"
+  location            = azurerm_resource_group.project.location
+  resource_group_name = azurerm_resource_group.project.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "bastion" {
+  name                = "bastion-nic"
+  location            = azurerm_resource_group.project.location
+  resource_group_name = azurerm_resource_group.project.name
+
+  ip_configuration {
+    name                          = "bastion-ip-configuration"
+    subnet_id                     = azurerm_subnet.public.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.bastion.id
+  }
+}
+
+resource "azurerm_virtual_machine" "bastion" {
+  name                  = "bastion"
+  location              = azurerm_resource_group.project.location
+  resource_group_name   = azurerm_resource_group.project.name
+  network_interface_ids = [azurerm_network_interface.bastion.id]
+  vm_size               = "Standard_B1ls"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "bastiondisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "bastion"
+    admin_username = "adminuser"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+      key_data = file("~/.ssh/id_rsa.pub")
+      path = "/home/adminuser/.ssh/authorized_keys"
+    }
+  }
 }
